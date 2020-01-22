2fceb05984ed9944de5782ec5158fac2ba98063f
TechnicalIllusion <robert_coones@yahoo.com>
2020-01-21 21:03:11 -0600

    added base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { RecipeItemComponent } from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
+import { ListComponent } from './shopping-list/list/list.component';
+import { ListEditComponent } from './shopping-list/list-edit/list-edit.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    ListComponent,
+    ListEditComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
